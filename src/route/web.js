02483cc1cfe7b388cfc343a8588d50366ef76507
dvh3152002 <dvh31502@gmail.com>
2022-12-22 21:08:04 +0700

    Áp Dụng Mô Hình MVC vào Dự Án Node.JS

--- src/route/web.js
+++ src/route/web.js
@@ -1,11 +1,10 @@
 import express from "express";
+import homeController from "../controllers/homeController";
 
 let router = express.Router();
 
 let initWebRoute = (app) => {
-    router.get("/", (req, res) => {
-        return res.send("Hello");
-    });
+    router.get("/", homeController.getHomePage);
     return app.use("/", router);
 }
 
