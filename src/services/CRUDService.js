cc41fe699474e7d6c2d12055aacf0965edca9695
dvh3152002 <dvh31502@gmail.com>
2022-12-23 10:45:06 +0700

    CRUD với Node.js và Sequelize - Sử Dụng ORM Để Tạo Dữ Liệu Mới Vào Database (Create) - Phần 1

---
+++ src/services/CRUDService.js
@@ -0,0 +1,40 @@
+import bcrypt from 'bcryptjs';
+import db from '../models';
+
+const salt = bcrypt.genSaltSync(10);
+
+let createNewUser = async (data) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let hashPassword = await hashUserPassword(data.password);
+            await db.User.create({
+                email: data.email,
+                password: hashPassword,
+                firstName: data.firstName,
+                lastName: data.lastName,
+                address: data.address,
+                phonenumber: data.phonenumber,
+                gender: data.gender === '1' ? true : false,
+                roleId: data.roleId
+            })
+            resolve('create new user success');
+        } catch (error) {
+            reject(error);
+        }
+    });
+}
+
+let hashUserPassword = (password) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let hash = await bcrypt.hashSync(password, salt);
+            resolve(hash)
+        } catch (error) {
+            reject(error);
+        }
+    })
+}
+
+module.exports = {
+    createNewUser: createNewUser
+}
